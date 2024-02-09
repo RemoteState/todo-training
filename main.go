4ec93bd72697352996789b18aff6e5c388901059
RemoteState <remotestate@RemoteStates-MacBook-Pro.local>
2024-02-09 18:40:35 +0530

    TODO with Auth

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"github.com/google/uuid"
@@ -13,6 +14,8 @@ import (
 	_ "github.com/lib/pq"
 )
 
+const userContextKey = "userContext"
+
 type todo struct {
 	Id          uuid.UUID  `json:"id,omitempty" db:"id"`
 	Description string     `json:"description,omitempty" db:"description"`
@@ -22,15 +25,54 @@ type todo struct {
 
 var todos = make([]todo, 0)
 
+func AuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		sessionToken := r.Header.Get("token")
+		userId, err := getUserIdForToken(sessionToken)
+		if err != nil {
+			RespondJSON(w, http.StatusUnauthorized, err)
+			return
+		}
+
+		r = r.WithContext(context.WithValue(r.Context(), userContextKey, userId))
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func getUserIdForToken(sessionToken string) (string, error) {
+	SQL := `select user_id from session where id = $1 and archived_at is null`
+	var userId string
+	err := DB.Get(&userId, SQL, sessionToken)
+	return userId, err
+}
+
 func health(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "health\n")
 }
 
+//
+//// LanguageMiddleWare this middle ware will take the language from header and add it to context
+//func LanguageMiddleWare(next http.Handler) http.Handler {
+//	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+//		lang := r.Header.Get("language")
+//		// check if the language provided is correct if not use english as default
+//		userLanguage := i18n.ValidateLanguage(lang)
+//		ctx := context.WithValue(r.Context(), LanguageCtx, userLanguage)
+//		next.ServeHTTP(w, r.WithContext(ctx))
+//		return
+//	})
+//}
+
 func todoList(w http.ResponseWriter, req *http.Request) {
+
+	//	r = r.WithContext(context.WithValue(r.Context(), userContextKey, userId))
+	userId := req.Context().Value(userContextKey).(string)
+
 	// language=SQL
-	SQL := "select id, description, created_at, completed_at from todo where archived_at is null"
+	SQL := "select id, description, created_at, completed_at from todo where archived_at is null and user_id = $1"
 	var todos []todo
-	err := DB.Select(&todos, SQL)
+	err := DB.Select(&todos, SQL, userId)
 	if err != nil {
 		fmt.Println("error reading todos %+v", err)
 		w.WriteHeader(http.StatusInternalServerError)
@@ -40,14 +82,16 @@ func todoList(w http.ResponseWriter, req *http.Request) {
 }
 
 func addTodo(w http.ResponseWriter, req *http.Request) {
+	userId := req.Context().Value(userContextKey).(string)
+
 	var body todo
 	if err := ParseBody(req.Body, &body); err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
-	SQL := `insert into todo (id, description, created_at) values ($1, $2, $3)`
-	_, err := DB.Queryx(SQL, uuid.New(), body.Description, time.Now())
+	SQL := `insert into todo (id, description, created_at, user_id) values ($1, $2, $3, $4)`
+	_, err := DB.Queryx(SQL, uuid.New(), body.Description, time.Now(), userId)
 	if err != nil {
 		fmt.Println("error writing todos %+v", err)
 		w.WriteHeader(http.StatusInternalServerError)
@@ -58,13 +102,15 @@ func addTodo(w http.ResponseWriter, req *http.Request) {
 }
 
 func updateTODO(w http.ResponseWriter, req *http.Request) {
+	userId := req.Context().Value(userContextKey).(string)
+
 	var body todo
 	if err := ParseBody(req.Body, &body); err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-	SQL := `update todo set description = $1 where id = $2`
-	_, err := DB.Queryx(SQL, body.Description, body.Id)
+	SQL := `update todo set description = $1 where id = $2 and user_id = $3`
+	_, err := DB.Queryx(SQL, body.Description, body.Id, userId)
 	if err != nil {
 		fmt.Println("error writing todos %+v", err)
 		w.WriteHeader(http.StatusInternalServerError)
@@ -74,14 +120,16 @@ func updateTODO(w http.ResponseWriter, req *http.Request) {
 }
 
 func completeTODO(w http.ResponseWriter, req *http.Request) {
+	userId := req.Context().Value(userContextKey).(string)
+
 	var body todo
 	if err := ParseBody(req.Body, &body); err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
-	SQL := `update todo set completed_at = $1 where id = $2`
-	_, err := DB.Queryx(SQL, time.Now(), body.Id)
+	SQL := `update todo set completed_at = $1 where id = $2 and user_id = $3`
+	_, err := DB.Queryx(SQL, time.Now(), body.Id, userId)
 	if err != nil {
 		fmt.Println("error writing todos %+v", err)
 		w.WriteHeader(http.StatusInternalServerError)
@@ -91,13 +139,14 @@ func completeTODO(w http.ResponseWriter, req *http.Request) {
 }
 
 func deleteTODO(w http.ResponseWriter, req *http.Request) {
+	userId := req.Context().Value(userContextKey).(string)
 	var body todo
 	if err := ParseBody(req.Body, &body); err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-	SQL := `update todo set archived_at = now() where id = $1`
-	_, err := DB.Queryx(SQL, body.Id)
+	SQL := `update todo set archived_at = now() where id = $1 and user_id = $2`
+	_, err := DB.Queryx(SQL, body.Id, userId)
 	if err != nil {
 		fmt.Println("error writing todos %+v", err)
 		w.WriteHeader(http.StatusInternalServerError)
@@ -134,7 +183,8 @@ func main() {
 		//r.Put("/update-todo", updateTODO)
 		//r.Put("/complete-todo", completeTODO)
 		//r.Delete("/delete-todo", deleteTODO)
-
+		r.Post("/login", login)
+		userRouter(r)
 		todoRouters(r)
 	})
 
@@ -142,8 +192,76 @@ func main() {
 	http.ListenAndServe(":8080", router)
 }
 
+/*
+browser -> request -> handler -> response -> browser
+browser -> request -> middleware -> handler -> middleware -> response -> browser
+*/
+type user struct {
+	Id          uuid.UUID  `json:"id,omitempty" db:"id"`
+	Name        string     `json:"name,omitempty" db:"name"`
+	Email       string     `json:"email,omitempty" db:"email"`
+	Password    string     `json:"password" db:"password"`
+	CreatedAt   *time.Time `json:"createdAt,omitempty" db:"created_at"`
+	CompletedAt *time.Time `json:"completedAt,omitempty" db:"completed_at"`
+}
+
+func addUser(w http.ResponseWriter, r *http.Request) {
+	var body user
+	if err := ParseBody(r.Body, &body); err != nil {
+		RespondJSON(w, http.StatusBadRequest, err)
+		return
+	}
+
+	SQL := `insert into users (id, name, email, password) values ($1, $2, $3, $4)`
+	_, err := DB.Queryx(SQL, uuid.New(), body.Name, body.Email, body.Password)
+	if err != nil {
+		RespondJSON(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	RespondJSON(w, http.StatusCreated, nil)
+}
+
+type loginRequest struct {
+	Email    string `json:"email,omitempty" db:"email"`
+	Password string `json:"password" db:"password"`
+}
+
+type response struct {
+	Token string `json:"token"`
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	var body loginRequest
+	if err := ParseBody(r.Body, &body); err != nil {
+		RespondJSON(w, http.StatusBadRequest, err)
+		return
+	}
+
+	SQL := `select id from users where email = $1 and password = $2`
+	var userId string
+	err := DB.Get(&userId, SQL, body.Email, body.Password)
+	if err != nil {
+		RespondJSON(w, http.StatusUnauthorized, err)
+		return
+	}
+
+	SQL = `insert into session (id, user_id) values ($1, $2)`
+	sessionId := uuid.New()
+	_, err = DB.Queryx(SQL, sessionId, userId)
+	if err != nil {
+		RespondJSON(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	RespondJSON(w, http.StatusCreated, response{
+		Token: sessionId.String(),
+	})
+}
+
 func todoRouters(r chi.Router) chi.Router {
 	return r.Route("/todo", func(todoRouter chi.Router) {
+		todoRouter.Use(AuthMiddleware)
 		todoRouter.Get("/", todoList)
 		todoRouter.Post("/", addTodo)
 		todoRouter.Route("/{id}", func(todoIdRouter chi.Router) {
@@ -151,6 +269,12 @@ func todoRouters(r chi.Router) chi.Router {
 			todoIdRouter.Put("/complete", completeTODO)
 			todoIdRouter.Delete("/", deleteTODO)
 		})
+	})
+}
+
+func userRouter(r chi.Router) chi.Router {
+	return r.Route("/user", func(todoRouter chi.Router) {
+		todoRouter.Post("/", addUser)
 	})
 }
 
@@ -180,12 +304,19 @@ func ParseBody(body io.Reader, out interface{}) error {
 }
 
 /*
+ */
+
+/*
+Add User -> register the user in the system
+Login -> User can login
+---------
 * TODO List -> GET 					http://localhost:8080/todo
 * Add TODO -> POST 					http://localhost:8080/todo
 * Update Description -> PUT 		http://localhost:8080/todo/:id
 * Mark todo completed -> PUT 		http://localhost:8080/todo/:id/complete
 * Delete -> DELETE 					http://localhost:8080/todo/:id
- */
+-------
+*/
 
 /*
 error reading todos %+v sql: Scan error on column index 2, name "created_at": unsupported Scan, storing driver.Value type <nil> into type *time.Time
